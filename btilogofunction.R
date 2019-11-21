e9cf8b86edf90922fe727cffcbe3ce0163661f9e
Jameson McBride <j.mcbride@columbia.edu>
2019-11-21 11:52:59 -0800

    Update btilogofunction.R

--- btilogofunction.R
+++ btilogofunction.R
@@ -1,10 +1,13 @@
 bti_colors <- c("#0d4459", "#00a990", "#d05527", "#a33332",
                 "#b381d0",  "#dfb9a6", "#b2b2b1", "#2A2A2A", "#ecc627")
 
+require(tidyverse); require(grid); require(magick)
+
+logo_grob <- grid::rasterGrob(magick::image_read("http://jrmcbride.github.io/btilogo.png"))
+
 bti_logo <- function(xmin, xmax, ymin, ymax){ # Takes input for logo dimensions, *relative to data*
-  require(tidyverse); require(grid); require(magick)
   list(
-    annotation_custom(grid::rasterGrob(magick::image_read("http://jrmcbride.github.io/btilogo.png")),
+    annotation_custom(logo_grob,
                       xmin, xmax, ymin, ymax),
     coord_cartesian(clip = "off"),
     theme_bw(), # Can be replaced with other themes
